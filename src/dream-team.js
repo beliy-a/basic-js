a633a3bb77043ddbba44297bb76e152944f3d248
Alex <beliy.san9l@ya.ru>
2020-02-24 11:50:15 +0300

    add solution dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members) || Array.isArray(members) && members.length < 1) return false;
+
+  let letters = '';
+
+  members.map(name => {
+    if (typeof name === 'string') {
+      return name.split(' ').filter(Boolean)[0];
+    }
+  }).filter(Boolean).forEach(letter => letters += letter[0].toUpperCase());
+
+  return letters.split('').sort().join('');
 };
