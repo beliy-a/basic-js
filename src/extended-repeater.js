f61e9e1aa2c54dd93fdd1fc5797909dc25984b8c
Alex <beliy.san9l@ya.ru>
2020-02-27 14:17:46 +0300

    add solution to extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,28 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+    let result = '';
+    let transformStr = String(str);
+
+    if (!options.repeatTimes) options.repeatTimes = 1;
+
+    for (let i = 0; i < options.repeatTimes; i++) {
+        result += transformStr;
+
+        if (options.addition !== undefined) {
+            if (options.additionRepeatTimes !== undefined) {
+                for (let k = 0; k < options.additionRepeatTimes; k++) {
+                    result += options.addition + options.additionSeparator;
+                }
+
+                result = result.slice(0, -(String(options.additionSeparator).length));
+
+            } else {
+                result += options.addition;
+            }
+        }
+
+        result += options.separator == undefined ? options.separator = '+' : options.separator;
+
+    }
+
+    return result.slice(0, -(options.separator.length));
+}
